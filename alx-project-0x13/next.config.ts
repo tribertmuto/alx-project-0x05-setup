342134920e54d27b5463547d577ccc5d31bca876
tribertmuto <tribertmuto@gmail.com>
2025-08-04 17:09:02 +0200

    changes

---
+++ alx-project-0x13/next.config.ts
@@ -0,0 +1,10 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  experimental: {
+    appDir: false
+  }
+};
+
+export default nextConfig;
